0fae8559a4e3ecef222282769b941616ef2f751d
amacha2626 <amacha2626@gmail.com>
2020-03-30 00:53:41 +0900

    fix

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,24 @@
+class RelationshipsController < ApplicationController
+  protect_from_forgery :except => [:index, :show, :create, :destroy]
+
+  def index
+    @relationships = Relationship.all
+    render 'index', formats: 'json', handlers: 'jbuilder'
+  end
+
+  def show
+    @relationship = Relationship.find(user_id: params[:id])
+    render 'show', formats: 'json', handlers: 'jbuilder'
+  end
+
+  def create
+    Relationship.new
+    Relationship.create(user_id: params[:user_id], follow_id: params[:follow_id])
+  end
+
+  def destroy
+    relationship = Relationship.find(params[:id])
+    relationship.delete
+  end
+
+end
