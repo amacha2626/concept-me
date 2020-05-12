2831f25c9b0d5bd8d85df6fcaf82b50a09d82435
amacha2626 <amacha2626@gmail.com>
2020-05-12 15:09:30 +0900

    fix csrf session

--- app/controllers/api/refresh_controller.rb
+++ app/controllers/api/refresh_controller.rb
@@ -1,5 +1,6 @@
 class Api::RefreshController < ApplicationController
   before_action :authorize_refresh_by_access_request!
+  protect_from_forgery
 
   def create
     session = JWTSessions::Session.new(payload: claimless_payload, refresh_by_access_allowed: true)
